e375ac5fa54b737d95c8f0d3b6780bca24b5daae
Tyler Chambers <me@tylerchambers.net>
2021-12-19 11:15:59 -0500

    update setup

--- setup.py
+++ setup.py
@@ -19,5 +19,14 @@ setup(
     url='https://github.com/tylerchambers/electrum-client',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires = ['pycoin']
+    install_requires = ['pycoin'],
+    keywords = ['bitcoin', 'electrum']
+    classifiers=[
+        'Development Status :: 3 - Alpha',      
+        'Intended Audience :: Developers',      
+        'Topic :: Internet',
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3', 
+        'Programming Language :: Python :: 3.8',
+    ],
 )
